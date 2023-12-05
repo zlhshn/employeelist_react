82822a4feeea66faad3fc96320a9eee433bf449c
zlhshn <zsahin4401@gmail.com>
2023-12-05 11:28:29 +0300

    start

---
+++ src/components/Employee.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import data from "../data";
+
+const Employee = () => {
+  const size = 5;
+  const [currentPage, setCurrentpage] = useState(1);
+  const startİndex = (currentPage - 1) * size;
+  const endİndex = startİndex + size;
+  const displayData = data.slice(startİndex, endİndex);
+
+  const handlePrev = () => {
+    setCurrentpage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNext = () => {
+    const totalPages = Math.ceil(data.length / size);
+    setCurrentpage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  return (
+    <>
+      <p className="size">
+        (Employees {startİndex + 1} to {Math.min(endİndex, data.length)})
+      </p>
+      <div className="person-container">
+        {displayData.map(({ id, name, age, image, email }) => (
+          <div className="person" key={id}>
+            <img src={image} alt={name} />
+            <div className="info">
+              <h3>{name}</h3>
+              <p>{email}</p>
+              <p>{age}</p>
+            </div>
+          </div>
+        ))}
+        <div className="btns">
+          <button onClick={handlePrev}>Prev</button>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Employee;
