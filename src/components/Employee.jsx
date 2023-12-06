c7eea82ae51921b1209bed638981dc9a31989477
Zeliha ŞAHİN <140537448+zlhshn@users.noreply.github.com>
2023-12-06 10:35:07 +0300

    Update Employee.jsx

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -2,25 +2,21 @@ import { useState } from "react";
 import data from "../data";
 
 const Employee = () => {
-  const size = 5;
-  const [currentPage, setCurrentpage] = useState(1);
-  const startİndex = (currentPage - 1) * size;
-  const endİndex = startİndex + size;
-  const displayData = data.slice(startİndex, endİndex);
+  const [index,setIndex] = useState(0)
+  const displayData = data.slice(index,index+5);
 
   const handlePrev = () => {
-    setCurrentpage((prevPage) => Math.max(prevPage - 1, 1));
+    setIndex(data.length - Math.abs(index-5)
   };
 
   const handleNext = () => {
-    const totalPages = Math.ceil(data.length / size);
-    setCurrentpage((prevPage) => (prevPage % totalPages) + 1);
+   setIndex((index+5)%data.length)
   };
 
   return (
     <>
       <p className="size">
-        ( Employees  {startİndex + 1} to {Math.min(endİndex, data.length)} )
+        ( Employees  {index+1} to {index+5}) )
       </p>
       <div className="person-container">
         {displayData.map(({ id, name, age, image, email }) => (
